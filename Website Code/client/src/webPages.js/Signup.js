7786f44c6c24fb99ad5a7e76969dca74255d6b1e
Joshua <joshuatanaka@gmail.com>
2024-08-05 23:25:57 -0700

    Signup MySQL Start

--- Website Code/client/src/webPages.js/Signup.js
+++ Website Code/client/src/webPages.js/Signup.js
@@ -4,19 +4,70 @@ import axios from 'axios';
 const Signup = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    
-    return (
-        <div>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <center><h class="title">Sign Up</h></center>
-            <br></br>
-            <center><ul><b>Username</b></ul></center>
-            <center><input type="text" placeholder="Enter Username" name="uname" required></input></center>
-            <br></br>
-            <center><ul><b>Password</b></ul></center>
-            <center><input type="password" placeholder="Enter Password" name="psw" required></input></center>
-            <br></br>
-            <center><button type="submit">Sign Up</button></center>
+    const [firstname, setFName] = useState('')
+    const [lastname, setLName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phonenumber, setPNum] = useState('')
+    const [gender, setGender] = useState('')
+    const [medicalStaff, setMedicalStaff] = useState(false)
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('http://localhost:4000/signup', {username: username, password: password, firstname: firstname, 
+            lastname:lastname, email:email, phonenumber:phonenumber, gender:gender, medicalStaff: medicalStaff
+        })
+        .then((data)=> {
+            console.log(data);
+            setUsername('');
+            setPassword('');
+            setFName('');
+            setLName('');
+            setEmail('');
+            setPNum('');
+            setGender('');
+            setMedicalStaff('');
+        })
+    }
+    return (       
+        <div id="gradient">
+            <br/><br/><br/><br/>
+            <center><h1 className="title">Register an Account</h1></center>
+            <br/>
+            <form onSubmit={handleSubmit}>
+                <center>
+                    <ul><b>Username</b></ul>
+                    <input type="text" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>Password</b></ul>
+                    <input type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>First Name</b></ul>
+                    <input type="text" placeholder="Enter your First Name" value={firstname} onChange={(e) => setFName(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>Last Name</b></ul>
+                    <input type="text" placeholder="Enter your Last Name" value={lastname} onChange={(e) => setLName(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>Email</b></ul>
+                    <input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>Phone Number</b></ul>
+                    <input type="tel" placeholder="Enter Phone Number" value={phonenumber} onChange={(e) => setPNum(e.target.value)} required />
+                    <br /><br />
+                    <ul><b>Gender</b></ul>
+                    <input type="text" placeholder="Enter Gender(M or F)" value={gender} onChange={(e) => setGender(e.target.value)} required />
+                    <br /><br />
+                    <label htmlFor="medicalStaff">
+                        <input 
+                            type="checkbox" 
+                            id="medicalStaff" 
+                            checked={medicalStaff} 
+                            onChange={(e) => setMedicalStaff(true)} 
+                        />
+                        Are you a Medical Staff?
+                    </label>
+                    <br /><br />
+                    <button type="submit">Sign Up</button>
+                </center>
+            </form>
         </div>        
     );
 }
--- Website Code/server/index.js
+++ Website Code/server/index.js
@@ -16,16 +16,44 @@ const pool = mysql.createPool({
     password: 'password',
     database: 'sjcs clinic'
 })
-app.post('/login', (req,res) => {
+// app.post('/login', (req,res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     pool.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result)=>{
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.send({username: username});
+//             res.send({password: password});
+
+//         }
+//     })
+    
+// })
+app.post('/signup', (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-    pool.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result)=>{
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const phonenumber = req.body.phonenumber;
+    const gender = req.body.gender;
+    const medicalStaff = req.body.medicalStaff ? 1: 0;
+
+
+    pool.query("INSERT INTO users (FirstName, LastName, SecurityLevel, UserName, UserPassword, Email, PhoneNumber, Gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [firstname, lastname, medicalStaff, username, password,  email, phonenumber, gender], (err, result)=>{
         if (err) {
             console.log(err);
         } else {
             res.send({username: username});
             res.send({password: password});
-
+            res.send({firstname: firstname});
+            res.send({lastname:lastname});
+            res.send({email:email});
+            res.send({phonenumber:phonenumber});
+            res.send({gender:gender});
+            res.send({medicalStaff: medicalStaff});
+            
         }
     })
     
