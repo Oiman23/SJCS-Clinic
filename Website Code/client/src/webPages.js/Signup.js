aedf68efa3293e9086b37390ce514fa6faec87dd
Jareuu <jerrydunguyen@gmail.com>
2024-08-05 19:58:23 -0700

    Update Signup.js

--- Website Code/client/src/webPages.js/Signup.js
+++ Website Code/client/src/webPages.js/Signup.js
@@ -7,8 +7,30 @@ const Signup = () => {
     const [firstname, setFName] = useState('')
     const [lastname, setLName] = useState('')
     const [email, setEmail] = useState('')
-    const [phonenumber, setpNum] = useState('')
+    const [phonenumber, setPNum] = useState('')
     const [gender, setGender] = useState('')
+    const [medicalStaff, setMedicalStaff] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:4000/login', {
+                username,
+                password,
+                firstname,
+                lastname,
+                email,
+                phonenumber,
+                gender,
+                medicalStaff
+            });
+            console.log(response.data);
+            // Handle successful signup (e.g., show a success message, redirect)
+        } catch (error) {
+            console.error('Error signing up:', error);
+            // Handle error (e.g., show error message)
+        }
+    };
     
     return (
         <html>
@@ -16,30 +38,42 @@ const Signup = () => {
                 <br></br><br></br><br></br><br></br>
                 <center><h class="title">Register an Account</h></center>
                 <br></br>
-                <center><ul><b>Username</b></ul></center>
-                <center><input type="text" placeholder="Enter Username" name="uname" required></input></center>
-                <br></br>
-                <center><ul><b>Password</b></ul></center>
-                <center><input type="password" placeholder="Enter Password" name="psw" required></input></center>
-                <br></br>
-                <center><ul><b>First Name</b></ul></center>
-                <center><input type="text" placeholder="Enter your First Name" name="fname" required></input></center>
-                <br></br>
-                <center><ul><b>Last Name</b></ul></center>
-                <center><input type="text" placeholder="Enter your Last Name" name="lname" required></input></center>
-                <br></br>
-                <center><ul><b>Email</b></ul></center>
-                <center><input type="text" placeholder="Enter Email" name="email" required></input></center>
-                <br></br>
-                <center><ul><b>Phone Number</b></ul></center>
-                <center><input type="text" placeholder="Enter Phone Number" name="pnum" required></input></center>
-                <br></br>
-                <center><ul><b>Gender</b></ul></center>
-                <center><input type="text" placeholder="Enter Gender(M or F)" name="gender" required></input></center>
-                <br></br>
-                <center><label for="medicalStaff">Are you a Medical Staff?</label><input type="checkbox" id="medicalStaff" name="medicalStaff" value="medicalStaff"/></center>
-                <br></br>
-                <center><button type="submit">Sign Up</button></center>
+                <form onSubmit={handleSubmit}>
+                    <center>
+                        <ul><b>Username</b></ul>
+                        <input type="text" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>Password</b></ul>
+                        <input type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>First Name</b></ul>
+                        <input type="text" placeholder="Enter your First Name" value={firstname} onChange={(e) => setFName(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>Last Name</b></ul>
+                        <input type="text" placeholder="Enter your Last Name" value={lastname} onChange={(e) => setLName(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>Email</b></ul>
+                        <input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>Phone Number</b></ul>
+                        <input type="tel" placeholder="Enter Phone Number" value={phonenumber} onChange={(e) => setPNum(e.target.value)} required />
+                        <br /><br />
+                        <ul><b>Gender</b></ul>
+                        <input type="text" placeholder="Enter Gender(M or F)" value={gender} onChange={(e) => setGender(e.target.value)} required />
+                        <br /><br />
+                        <label htmlFor="medicalStaff">
+                            <input 
+                                type="checkbox" 
+                                id="medicalStaff" 
+                                checked={medicalStaff} 
+                                onChange={(e) => setMedicalStaff(e.target.checked)} 
+                            />
+                            Are you a Medical Staff?
+                        </label>
+                        <br /><br />
+                        <button type="submit">Sign Up</button>
+                    </center>
+                </form>
             </div>        
         </html>
     );
