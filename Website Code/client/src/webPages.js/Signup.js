8cfae780d02fec471e1d23f50272b3811f65e9d1
Jareuu <jerrydunguyen@gmail.com>
2024-08-05 18:27:54 -0700

    Update Signup.js

--- Website Code/client/src/webPages.js/Signup.js
+++ Website Code/client/src/webPages.js/Signup.js
@@ -7,14 +7,29 @@ const Signup = () => {
     
     return (
         <div>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-            <center><h class="title">Sign Up</h></center>
+            <br></br><br></br><br></br><br></br><br></br><br></br>
+            <center><h class="title">Register an Account</h></center>
             <br></br>
-            <center><ul><b>Username</b></ul></center>
+            <center><ul><b>Username:</b></ul></center>
             <center><input type="text" placeholder="Enter Username" name="uname" required></input></center>
             <br></br>
-            <center><ul><b>Password</b></ul></center>
+            <center><ul><b>Password:</b></ul></center>
             <center><input type="password" placeholder="Enter Password" name="psw" required></input></center>
+            <br></br>
+            <center><ul><b>First Name:</b></ul></center>
+            <center><input type="text" placeholder="Enter your First Name" name="fname" required></input></center>
+            <br></br>
+            <center><ul><b>Last Name:</b></ul></center>
+            <center><input type="text" placeholder="Enter your Last Name" name="lname" required></input></center>
+            <br></br>
+            <center><ul><b>Email:</b></ul></center>
+            <center><input type="text" placeholder="Enter Email" name="email" required></input></center>
+            <br></br>
+            <center><ul><b>Phone Number:</b></ul></center>
+            <center><input type="text" placeholder="Enter Phone Number" name="pnum" required></input></center>
+            <br></br>
+            <center><ul><b>Gender:</b></ul></center>
+            <center><input type="text" placeholder="Enter Gender(M or F)" name="gender" required></input></center>
             <br></br>
             <center><button type="submit">Sign Up</button></center>
         </div>        
