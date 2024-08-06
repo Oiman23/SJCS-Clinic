6b29fa4aed3abe303d3f0cec9e491135f3990fa0
Joshua <joshuatanaka@gmail.com>
2024-08-05 19:57:07 -0700

    sql connection

--- Website Code/client/src/webPages.js/RootLay.js
+++ Website Code/client/src/webPages.js/RootLay.js
@@ -3,9 +3,8 @@ import { Outlet } from "react-router-dom";
 const RootLay = () => {
     return (
         <div>
-            <header><Navigation/></header>
-            <body><Outlet/></body>
-            
+            <Navigation/>
+            <Outlet/>
         </div>        
     );
 }
--- Website Code/server/index.js
+++ Website Code/server/index.js
@@ -2,27 +2,34 @@ const express = require ('express');
 const app = express();
 const cors = require ('cors');
 const mysql = require ('mysql')
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
 
 app.use(cors());
 
-var pool = mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'sjcs clinic'
 })
-app.get('/', (req, res) => { 
-    pool.query("INSERT INTO users (username, password) VALUES ('Testing1', '3123123123')", (err, result) =>{
-        if (err){
+app.post('/login', (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    pool.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result)=>{
+        if (err) {
             console.log(err);
         } else {
-            console.log(result);
+            res.send({username: username});
+            res.send({password: password});
+
         }
     })
-})  
-
+    
+})
 app.listen(4000, () => {
     console.log('server listening on port 4000');
 })
