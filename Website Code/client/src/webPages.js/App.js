789abd39884064761c5a3f69d219edad6e953914
Jareuu <jerrydunguyen@gmail.com>
2024-08-06 14:26:19 -0700

    App.css and App.js modifications

--- Website Code/client/src/webPages.js/App.js
+++ Website Code/client/src/webPages.js/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="App">
       <div id = "gradient">
-        Homepage
+        <center><h1 className="title">Welcome to SJCS Clinic</h1></center>
+        <center><img src="https://media.istockphoto.com/id/1194838627/vector/patient-in-hospital.jpg?s=612x612&w=0&k=20&c=LqhY8qXr1IgGA0PjGLwqEyVJL-MBTFBU5rf3Dcg4DWo="></img></center>
+        <center><p>Here at SJCS Clinic, we strive to deliver excellent service and proper treatment to any forms of illness. Our nurses, doctors, and surgeons do their very best to help patients stay at their best.</p></center>
+        <center><img src="https://media.istockphoto.com/id/621576732/vector/medical-team-hospital-staff-vector-illustration.jpg?s=612x612&w=0&k=20&c=-ouwunIZSePve5XumMkGTB1zBRX34byPskCd-T2hQWM="></img></center>
+        <center><p>You are currently on our Homepage. Select Login if you have an account or Signup if you'd like to register.</p></center>
+        <br/>
       </div>
     </div>
   )
